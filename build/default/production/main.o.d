aca385d1327a2222a41475a1ecd5e057ab6676f7
coopb <coopb@LAPTOP-SI3TQ58U>
2023-04-07 16:11:43 -0500

    removed interrupt_based_button.c

---
+++ build/default/production/main.o.d
@@ -0,0 +1,3 @@
+build/default/production/main.o: main.c dac.h
+
+dac.h:
---
+++ dac.h
@@ -0,0 +1,24 @@
+/* 
+ * File:   dac.h
+ * Author: coopb
+ *
+ * Created on April 7, 2023, 3:58 PM
+ */
+
+#ifndef DAC_H
+#define	DAC_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+void init_DAC(void);
+
+void write_DAC(void);
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* DAC_H */
+
