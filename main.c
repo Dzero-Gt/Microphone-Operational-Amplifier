7e6e8be1a62353c23d373bbf483632c955a32074
coopb <coopb@10.131.238.247>
2023-04-14 17:16:07 -0500

    merging main.c

--- main.c
+++ main.c
@@ -4,19 +4,12 @@
  *
  * Created on March 15, 2023, 8:26 PM
  */
-
-/*
- * currently this is a basic program that can be adapted later
- * initializes button to pin rb8
- * it uses IC1 interrupt to recognize button input
- */
-
-
 #include "xc.h"
 #include <stdlib.h>
 #include "button.h"
 #include "ADC_config.h"
 #include "dac.h"
+#include "ADC_config.h"
 //
 // CW1: FLASH CONFIGURATION WORD 1 (see PIC24 Family Reference Manual 24.1)
 #pragma config ICS = PGx1          // Comm Channel Select (Emulator EMUC1/EMUD1 pins are shared with PGC1/PGD1)
@@ -34,10 +27,18 @@
                                        // Fail-Safe Clock Monitor is enabled)
 #pragma config FNOSC = FRCPLL      // Oscillator Select (Fast RC Oscillator with PLL module (FRCPLL))
 //
+//button interrupt code
+volatile int State = 0; // state:0  use raw input 
+                        // state:1  use filtered data
+volatile unsigned int overflow=0; // overflow occurs after one second
+volatile unsigned long int time_current_click=0;
+volatile unsigned long int time_preivious_click=0;
 
-void setup();
 
-
+void __attribute__((interrupt, auto_psv)) _T2Interrupt(void) {
+    _T2IF = 0; // turn the interrupt flag back to 0
+    overflow++; // and increase the overflow by one
+}
 
 void __attribute__((__interrupt__,__auto_psv__)) _SPI2Interrupt(void)
 {
@@ -55,6 +56,16 @@ void __attribute__((__interrupt__,__auto_psv__)) _SPI2Interrupt(void)
     LATBbits.LATB6 = 1;
 }
 
+void __attribute__((__interrupt__,__auto_psv__)) _IC1Interrupt(void){
+
+    
+    _IC1IF = 0; // reset the IC interrupt flag
+    time_preivious_click=time_current_click;
+    time_current_click = (unsigned long int)((unsigned long int)IC1BUF + (unsigned long int)overflow*(PR2+1));
+    if((time_current_click - time_preivious_click) >125){ //to achieve a 2ms debounce delay must find (2/1000)(PR2+1)=125
+        State = 1-State; // we intend to use the button as a toggle between the raw and filtered input
+    }
+}
 // delays in one ms increments
 void delay(unsigned int ms) {
     int i;
@@ -64,23 +75,30 @@ void delay(unsigned int ms) {
     }
     return;
 }
-
-
-
 void setup(){
     
     AD1PCFG = 0xffff;     //sets all pins to digital I/O
     CLKDIVbits.RCDIV=0;   // set frequency to 16 MHz 
 }
 
+void __attribute__ ((__interrupt__, __auto_psv__)) _ADC1Interrupt(void){
+    IFS0bits.AD1IF = 0;
+    int data = ADC1BUF0;
+    write_DAC(data);
+    // include conversion data here
+}
+
 int main(void) {
     setup();
     init_button();
     init_DAC();
+    ADC_init();
     
 
-    while (1);
+    while (1){}
     
     return -1;
 }
+
+
 
